d50b590316c12d875448295c3b2537c173f86343
AlejoMaik <103090286+AlejoMaik@users.noreply.github.com>
2023-01-23 11:43:11 -0500

    Add files via upload

---
+++ Bronze/Team.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n; cin >> n;
+    int t = 0;
+    for (int i = 0; i < n; ++i) {
+        int a, b, c; cin >> a >> b >> c;
+        if ((a+b+c) > 1) {
+            t++;
+        }
+    }
+
+    cout<<t;
+
+
+    return 0;
+
+}
